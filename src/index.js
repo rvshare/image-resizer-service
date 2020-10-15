00630f50cf591a64d27526b2998204ef89693f20
Curtis Autery <curtis@rvshare.com>
2020-10-15 14:49:35 -0400

    Use cloudinary-style resize params in URL

--- src/index.js
+++ src/index.js
@@ -3,32 +3,37 @@ const {errorResponse} = require("./response");
 const {original, resize} = require("./image");
 
 exports.handler = (event) => new Promise((resolve, reject) => {
-    const imageBucket = process.env.IMAGE_BUCKET;
+  const imageBucket = process.env.IMAGE_BUCKET;
 
-    if (!imageBucket) {
-        return reject(`Error: Set environment variable IMAGE_BUCKET`);
-    }
+  if (!imageBucket) {
+    return reject(`Error: Set environment variable IMAGE_BUCKET`);
+  }
 
-    const path = event.path;
-    const objectKey = decodeURI(url.parse(path).pathname.replace(/^\/+/g, ''));
-    console.log('INFO: key: ' + objectKey);
+  const path = event.path.replace(/\/([a-z]_[\w:]+,)*([a-z]_[\w:]+)\//,'/');
+  const objectKey = decodeURI(url.parse(path).pathname.replace(/^\/+/g, ''));
+  const size = {}
+  const match = event.path.match(/\/(([a-z]_[\w:]+,)*([a-z]_[\w:]+))\//)
+  if (match && match[1]) {
+    match[1].split(',').forEach(pair => {
+      const [k, v] = pair.split('_')
+      size[k] = v
+    })
+  }
 
-    const queryParameters = event.queryStringParameters || {};
+  if (!size.w && !size.h) {
+    return original(imageBucket, objectKey)
+      .then(resolve)
+      .catch(reject);
+  }
 
-    if (!queryParameters.width && !queryParameters.height) {
-        return original(imageBucket, objectKey)
-            .then(resolve)
-            .catch(reject);
-    }
+  const width = parseInt(size.w);
+  const height = parseInt(size.h);
 
-    const width = parseInt(queryParameters.width);
-    const height = parseInt(queryParameters.height);
+  if ((size.w && isNaN(width)) || (size.h && isNaN(height))) {
+    return reject(errorResponse(`width and height parameters must be integer`, 400));
+  }
 
-    if ((queryParameters.width && isNaN(width)) || (queryParameters.height && isNaN(height))) {
-        return reject(errorResponse(`width and height parameters must be integer`, 400));
-    }
-
-    return resize(imageBucket, objectKey, width, height)
-        .then(resolve)
-        .catch(reject);
+  return resize(imageBucket, objectKey, width, height)
+    .then(resolve)
+    .catch(reject);
 });
